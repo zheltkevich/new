0949ab08200806ac879c884f1bc4391f55f5f8c7
Aleksandr <zheltkevich42@gmail.com>
2022-11-12 21:16:44 +0300

    Config deploy

---
+++ scripts/deploy-vercel.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+git checkout --orphan production
+npm run build
+git --work-tree add --all
+git --work-tree commit -m production
+git push origin HEAD:production --force
+git checkout -f master
+git branch -D production
