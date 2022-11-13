2a75e95e6bdc557ae60816eff37c4324f9c0a430
Aleksandr <zheltkevich42@gmail.com>
2022-11-13 17:13:28 +0300

    Setup pwa install button

--- src/vue/components/pwa/PwaInstallButton.vue
+++ src/vue/components/pwa/PwaInstallButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        ref="pwaInstallButton"
+        id="install-button"
         class="pwa-install-button"
         type="button">
         Install App
@@ -8,14 +8,15 @@
 </template>
 
 <script>
-import setupInstallButton from '@pwa/setupInstallButton.js';
+// import setupInstallButton from '@pwa/setupInstallButton.js';
+import '@pwa/pwaInstall.js';
 export default {
     name: 'PwaInstallButton',
-    mounted() {
-        // eslint-disable-next-line no-console
-        console.log('mounted: ', this.$refs.pwaInstallButton);
+    // mounted() {
+    //     // eslint-disable-next-line no-console
+    //     console.log('mounted: ', this.$refs.pwaInstallButton);
 
-        if (this.$refs.pwaInstallButton) setupInstallButton(this.$refs.pwaInstallButton);
-    },
+    //     if (this.$refs.pwaInstallButton) setupInstallButton(this.$refs.pwaInstallButton);
+    // },
 };
 </script>
